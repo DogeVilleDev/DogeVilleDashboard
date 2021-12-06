fbbb8b04f9fb5ddab6a391830a64955aaa9610c2
DogeVilleDev <contact@dogeville.finance>
2021-12-06 19:25:55 +0100

    Updates

---
+++ precache-manifest.3e9cf3094225e7e94cdd3ddeafa2fd72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2126c21cb6484b74eed",
+    "url": "/DogeVilleDashboard/static/css/main.6f4c1333.chunk.css"
+  },
+  {
+    "revision": "a2126c21cb6484b74eed",
+    "url": "/DogeVilleDashboard/static/js/main.a2126c21.chunk.js"
+  },
+  {
+    "revision": "117d6c6acf6d34c41bc6",
+    "url": "/DogeVilleDashboard/static/css/1.57ef1573.chunk.css"
+  },
+  {
+    "revision": "117d6c6acf6d34c41bc6",
+    "url": "/DogeVilleDashboard/static/js/1.117d6c6a.chunk.js"
+  },
+  {
+    "revision": "f5f3f71c2f434dba5452",
+    "url": "/DogeVilleDashboard/static/js/runtime~main.f5f3f71c.js"
+  },
+  {
+    "revision": "446360269a67e8380f62a82fb2d4609b",
+    "url": "/DogeVilleDashboard/static/media/dollar.44636026.png"
+  },
+  {
+    "revision": "97c650c298aa71bf39ffbb9e875f1b6a",
+    "url": "/DogeVilleDashboard/static/media/DogeVilleLogo.97c650c2.png"
+  },
+  {
+    "revision": "a87216b5b29e510f191614cbdec3b33e",
+    "url": "/DogeVilleDashboard/static/media/Landscape2.a87216b5.jpg"
+  },
+  {
+    "revision": "980807edb2a7a5959a67a5897b2071a5",
+    "url": "/DogeVilleDashboard/index.html"
+  }
+];
